64b9e49ab512351522e56d6a7903f12cd2f0b41a
Simpe0518 <eriksimon31@gmail.com>
2023-12-01 15:03:19 +0100

    Add files via upload

---
+++ d1.py
@@ -0,0 +1,28 @@
+from asyncio.windows_events import NULL
+
+
+file = open("d1.txt", "r")
+numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+first = NULL
+last = NULL
+tot = 0
+for line in file.readlines():
+    print(line)
+    for digit in range(len(numbers)):
+        line = line.replace(numbers[digit],  numbers[digit] + str(digit+1) + numbers[digit])
+    print(line)
+
+    for i in line:
+        if (i.isdigit()):
+            last = i
+            if (first == NULL):
+                first = i
+        
+    #print(str(first) + str(last))    
+    tot += int(str(first) + str(last))
+    last = NULL
+    first = NULL
+
+print(tot)
+        
