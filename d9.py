1e5a3e3ab5e5b772468a6e7a71668f5fcab3d2b3
siman492 <siman492@student.liu.se>
2023-12-12 18:35:33 +0100

    cleanup d9

--- d9.py
+++ d9.py
@@ -11,29 +11,20 @@ def diff(history):
         new.append(history[i] - (history[i-1]))
     return new
 
-### part 1 ###
-def generate_tree1(history):
-    history.append(diff(history[-1]))
-    if history[-1].count(history[-1][0]) == len(history[-1]):
-        for i in range(len(history)-1, 0, -1):
-            history[i-1].append(history[i][-1] + history[i-1][-1])
-        return history[0][-1]
-    else:
-        return generate_tree1(history)
-    
-### part 2 ###
-def generate_tree2(history):
+def generate_tree(history):
     history.append(diff(history[-1]))
     if history[-1].count(history[-1][0]) == len(history[-1]):
         for i in range(len(history)-1, 0, -1):
             history[i-1].insert(0, (history[i-1][0] - history[i][0]))
-        return history[0][0]
+            history[i-1].append(history[i][-1] + history[i-1][-1])
+        return history[0][-1], history[0][0]
     else:
-        return generate_tree2(history)
+        return generate_tree(history)
 
 for line in file:
-   tot1 += generate_tree1(line)
-   tot2 += generate_tree2(line)
+   part1, part2 = generate_tree(line)
+   tot1 += part1
+   tot2 += part2
 
 print(tot1)
 print(tot2)
