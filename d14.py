d6c4ecde2248890dcdb8c365be41458e2f016c9d
siman492 <siman492@student.liu.se>
2023-12-14 21:54:28 +0100

    14 done

--- d14.py
+++ d14.py
@@ -1,5 +1,7 @@
 file = open("d14.txt", "r")
 file = [[c for c in line.strip()] for line in file]
+beam_loads = []
+cntr = 0
 
 def rock_ahead(file, r, c):
     if file[r-1][c] == "O" or file[r-1][c] == "#" or r == 0:
@@ -27,9 +29,23 @@ def count_rock(file):
         tot += file[r].count("O") * (len(file) - r)
     return tot
 
+#--- part 1 ---
+print(count_rock(fall_north(file)))
 
-file = fall_north(file)
-for line in file:
-    print("".join(line).strip())
+#--- part 2 ---
+while(True):
+    for i in range(4):
+        file = fall_north(file)
+        rotat = list(zip(*reversed(file)))
+        file = [list(line) for line in rotat]
 
-print(count_rock(file))
+    load = count_rock(file)
+    cntr += 1
+
+    if load in beam_loads:
+        beam_loads = beam_loads[beam_loads.index(load):]
+        l = len(beam_loads)
+        print(beam_loads[(1000000000 - cntr) % l])
+        break
+
+    beam_loads.append(load)
